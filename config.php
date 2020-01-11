de7b7b6eaec134eb47ce012e825002e11e07d393
Fadil <rosunmungurudm@gmail.com>
2020-01-12 00:52:34 +0400

    Source codes

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'baseUrl' 		=> '',
+    'production' 	=> false,
+    'path'          => '{language}/{type}/{-title}',
+    'author'        => 'FADILXCODER',
+    'collections' 	=> [
+            'posts-fr'  => [
+                'type'      => 'post',
+                'language'  => 'fr',
+            ],
+            'posts-en'  => [
+                'type'      => 'post',
+                'language'  => 'en',
+            ],
+    ],
+	'contact_email' => 'fadil@xcoder.developer',
+];
---
+++ source/_layouts/post.blade.php
@@ -0,0 +1,7 @@
+@extends('_layouts.master')
+
+@section('body')
+    <h1>{{ $page->title }}</h1>
+    <p>By {{ $page->author }} - {{ date('F j, Y', $page->date) }}</p>
+    @yield('content')
+@endsection
